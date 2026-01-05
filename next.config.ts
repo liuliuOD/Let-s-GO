64acb87372f4f0c2cb3bb47682e92d0675045c45
liuliuOD <liuliugit@gmail.com>
2026-01-05 22:35:45 +0800

    feat: modify basePath for GitHub page deployment

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
   },
   // 3. 如果你的 GitHub Repo 名稱不是你的帳號名 (username.github.io)，
   // 而是像 "my-slot-machine"，請取消下面這行的註解並填入 repo 名稱：
-  basePath: '/let-s-go',
+  basePath: '/Let-s-GO',
 };
 
 export default nextConfig;
